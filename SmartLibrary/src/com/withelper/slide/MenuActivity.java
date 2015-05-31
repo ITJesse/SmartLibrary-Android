4cb83ed1dfb0cbb8382223b0f38a810039f5b313
itjesse <jesse@itjesse.cn>
2015-06-01 04:27:34 +0800

    删除消息中心

--- SmartLibrary/src/com/withelper/slide/MenuActivity.java
+++ SmartLibrary/src/com/withelper/slide/MenuActivity.java
@@ -77,32 +77,32 @@ public class MenuActivity extends FragmentActivity implements View.OnClickListen
         itemIndex = new ResideMenuItem(this, R.drawable.l_about, "馆区服务");
         itemBorrow = new ResideMenuItem(this, R.drawable.l_logout,  "我的借阅");
         itemLearning = new ResideMenuItem(this, R.drawable.i_sig,  "预约自习室");
-        itemNotice = new ResideMenuItem(this, R.drawable.l_con, "消息中心");
+//        itemNotice = new ResideMenuItem(this, R.drawable.l_con, "消息中心");
         itemQRcode = new ResideMenuItem(this, R.drawable.l_notice, "生成二维码");
         itemSet = new ResideMenuItem(this, R.drawable.l_set, "软件设置");
         
         itemIndex.setOnClickListener(this);
         itemBorrow.setOnClickListener(this);
         itemLearning.setOnClickListener(this);
-        itemNotice.setOnClickListener(this);
+//        itemNotice.setOnClickListener(this);
         itemQRcode.setOnClickListener(this);
         itemSet.setOnClickListener(this);
         
         resideMenu.addMenuItem(itemIndex, ResideMenu.DIRECTION_LEFT);
         resideMenu.addMenuItem(itemBorrow, ResideMenu.DIRECTION_LEFT);
         resideMenu.addMenuItem(itemLearning, ResideMenu.DIRECTION_LEFT);
-        resideMenu.addMenuItem(itemNotice, ResideMenu.DIRECTION_LEFT);
+//        resideMenu.addMenuItem(itemNotice, ResideMenu.DIRECTION_LEFT);
         resideMenu.addMenuItem(itemQRcode, ResideMenu.DIRECTION_LEFT);
         resideMenu.addMenuItem(itemSet, ResideMenu.DIRECTION_LEFT);
         
         // You can disable a direction by setting ->
         resideMenu.setSwipeDirectionDisable(ResideMenu.DIRECTION_RIGHT);
-/*        findViewById(R.id.title_bar_left_menu).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.title_bar_left_menu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 resideMenu.openMenu(ResideMenu.DIRECTION_LEFT);
             }
-        });*/
+        });
     }
 
 
@@ -113,27 +113,6 @@ public class MenuActivity extends FragmentActivity implements View.OnClickListen
 
     @Override
     public void onClick(View view) {
-/*    	switch(view.getId())
-		{
-		case R.id.testa:
-			Toast.makeText(getApplicationContext(), "2444",
-				     Toast.LENGTH_SHORT).show();
-			break;
-		case R.id.index_act:
-			Toast.makeText(getApplicationContext(), "2444",
-				     Toast.LENGTH_SHORT).show();
-			break;
-		case R.id.index_navi:
-			Toast.makeText(getApplicationContext(), "2555",
-				     Toast.LENGTH_SHORT).show();
-			break;
-		case R.id.index_book:
-			Toast.makeText(getApplicationContext(), "2666",
-				     Toast.LENGTH_SHORT).show();
-			break;
-			default:
-				break;
-		}*/
         if (view == itemBorrow){
             changeFragment(new BorrowFragment());
             title = (TextView) findViewById(R.id.titlebar_tv);
@@ -142,10 +121,10 @@ public class MenuActivity extends FragmentActivity implements View.OnClickListen
             changeFragment(new LearningFragment());
             title = (TextView) findViewById(R.id.titlebar_tv);
     		title.setText(R.string.menu3);
-        }else if (view == itemNotice){
-            changeFragment(new NoticeFragment());
-            title = (TextView) findViewById(R.id.titlebar_tv);
-    		title.setText(R.string.menu4);
+//        }else if (view == itemNotice){
+//            changeFragment(new NoticeFragment());
+//            title = (TextView) findViewById(R.id.titlebar_tv);
+//    		title.setText(R.string.menu4);
         }else if (view == itemQRcode){
             changeFragment(new SuggestionFragment());
             title = (TextView) findViewById(R.id.titlebar_tv);
